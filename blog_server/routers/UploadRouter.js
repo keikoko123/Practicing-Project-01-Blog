a76b875a03330d1d2372d84be18f7c53f76cd1ba
KEI <68806092+keikoko123@users.noreply.github.com>
2024-07-08 18:43:58 +0800

    Update UploadRouter.js

--- blog_server/routers/UploadRouter.js
+++ blog_server/routers/UploadRouter.js
@@ -2,45 +2,49 @@ const express = require("express");
 
 const router = express.Router();
 
-const fs = require("fs");
-
 const { blogDB, genID } = require("../db/DbUtils");
+
+const fs = require("fs");
 
 router.post("/rich_editor_upload", async (req, res) => {
 
+    console.log(req.files)
     if (!req.files) {
         res.send({
             "errno": 1, // 只要不等于 0 就行
             "message": "失败信息"
         })
-        return;
+        return
     }
 
-    let files = req.files;
-    let ret_files = [];
+    let files = req.files
+    let ret_files = []
 
     for (let file of files) {
-        //获取文件名字后缀
-        let file_ext = file.originalname.substring(file.originalname.lastIndexOf(".") + 1)
-        //随机文件名字
+        // file suffix
+        let file_ext = file.originalname.substring(file.originalname.lastIndexOf('.') + 1) //aa..a.jpg
+        // random file name
         let file_name = genID.NextId() + "." + file_ext
 
-        //修改名字加移动文件
+        //執行 req file完成後 實際上是在儲存的位置
         fs.renameSync(
             process.cwd() + "/public/uploads/temp/" + file.filename,
             process.cwd() + "/public/uploads/" + file_name
         )
+
+        //返回去 res file地址---------------------------------------------
         ret_files.push("/uploads/" + file_name)
+
     }
 
     res.send({
-        "errno": 0, // 注意：值是数字，不能是字符串
-        "data": {
-            "url": ret_files[0], // 图片 src ，必须
+        code: 200,
+        data: {
+            "url": ret_files[0]
         }
     })
-
 })
 
 
-module.exports = router
+
+module.exports = router;
